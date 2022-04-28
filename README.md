5ac0d4e22b5bcd08c06db71d707e40a36048a22f
ayadraganova <ayadraganova@gmail.com>
2022-04-28 22:40:27 +0200

    cm complete

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # cnn-keras
+
+This project is using Keras, a neural network API written in Python and integrated with TensorFlow, to classify dogs and cats.
+It is build and trained on convolutional neural networks (CNNs)
+
+## Prepare and Process Data 
+
+To train our keras model we are using the following [dataset from kaggle](https://www.kaggle.com/competitions/dogs-vs-cats/overview)
+
+
+## Build & Train the Model
+
+
+Training the model using the Adam Optimizer
+![sample](batch-sample.png)
+
+
+## Predicting 
+
+Preditcting our test data
+
+Example CM Output using PLT
+![matrix](cm.png)
+
+
+
+
+
+## Literature
+Bases on https://www.youtube.com/watch?v=qFJeN9V1ZsI
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import tensorflow as tf
-
 import matplotlib.pyplot as plt
+import numpy as np
+import itertools
 from keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import confusion_matrix
 
-# initializing the cnn
-classifier = Sequential()
-
+'''
+Preparing Test & Training data
+'''
 train_path = 'train'
 test_path = 'test'
 
@@ -34,5 +38,65 @@ def plotImages(images_arr):
     plt.show()
 
 
-plotImages(imgs)
-print(labels)
+# plotImages(imgs)
+# print(labels)
+
+'''
+Build and train the CNN
+'''
+model = Sequential(
+    [Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)),
+     MaxPool2D(pool_size=(2, 2), strides=2),
+     Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+     MaxPool2D(pool_size=(2, 2), strides=2),
+     Flatten(),
+     Dense(units=2, activation='softmax')])
+
+model.summary()
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=train_batches, epochs=10, verbose=2)
+
+'''
+Predict
+'''
+test_imgs, test_labels = next(test_batches)
+plotImages(test_imgs)
+print(test_labels)
+test_batches.classes
+
+predictions = model.predict(x=test_batches, verbose=0)
+np.round(predictions)
+cm = confusion_matrix(y_true=test_batches.classes, y_pred=np.argmax(predictions, axis=-1))
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion Matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+test_batches.class_indices
+
+cm_plot_labels = ['cat', 'dog']
+
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels)
